85a6e71d860cb2c406f0399a316ef460f1e7208a
Alexander Vasilev <46332577+rayonnant@users.noreply.github.com>
2026-02-27 20:01:35 +0300

    ci: add firebase support

--- vite.config.ts
+++ vite.config.ts
@@ -1,66 +1,69 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from "vite-plugin-pwa"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), VitePWA({
-    registerType: "autoUpdate",
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-    },
-    manifest: {
-      "short_name": "Rick and Morty",
-      "name": "Rick and Morty",
-      "start_url": ".",
-      "display": "standalone",
-      "theme_color": "#f8fbff",
-      "background_color": "#f8fbff",
-      "orientation": "portrait-primary",
-      "icons": [
-        {
-          "src": "/icons/icon-48x48.png",
-          "sizes": "48x48",
-          "type": "image/png"
-        },
-        {
-          "src": "/icons/icon-72x72.png",
-          "sizes": "72x72",
-          "type": "image/png"
-        },
-        {
-          "src": "/icons/icon-96x96.png",
-          "sizes": "96x96",
-          "type": "image/png"
-        },
-        {
-          "src": "/icons/icon-128x128.png",
-          "sizes": "128x128",
-          "type": "image/png"
-        },
-        {
-          "src": "/icons/icon-144x144.png",
-          "sizes": "144x144",
-          "type": "image/png"
-        },
-        {
-          "src": "/icons/icon-152x152.png",
-          "sizes": "152x152",
-          "type": "image/png"
-        },
-        {
-          "src": "/icons/icon-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png",
-          "purpose": "any maskable"
-        },
-        {
-          "src": "/icons/icon-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png"
-        }
-      ]
-    }
-  })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        short_name: 'Rick and Morty',
+        name: 'Rick and Morty',
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#f8fbff',
+        background_color: '#f8fbff',
+        orientation: 'portrait-primary',
+        icons: [
+          {
+            src: '/icons/icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
